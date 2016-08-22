7cb207af15ce221337f4192870d0ed9435427c47
ToruNiina <niina.toru.68u@gmail.com>
2016-08-22 15:04:56 +0900

    move utf related function to other file

--- src/parse_value.hpp
+++ src/parse_value.hpp
@@ -2,6 +2,7 @@
 #define TOML_PARSE_VALUE
 #include "definitions.hpp"
 #include "toml_values.hpp"
+#include "utfcodepoint.hpp"
 #include "is.hpp"
 #include <algorithm>
 
@@ -256,89 +257,6 @@ split_key_value(const std::basic_string<charT, traits, alloc>& str)
     return std::make_pair(key, value);
 }
 
-template<typename charT, typename traits, typename alloc>
-std::basic_string<charT, traits, alloc>
-utf8_to_char(const std::basic_string<charT, traits, alloc>& str)
-{
-    uint32_t codepoint;
-    std::basic_istringstream<charT, traits, alloc> iss(str);
-    iss >> std::hex >> codepoint;
-
-    std::basic_string<charT, traits, alloc> charactor;
-    if(codepoint < 0x80)
-    {
-        charactor += static_cast<unsigned char>(codepoint);
-    }
-    else if(codepoint < 0x800)
-    {
-        charactor += (static_cast<unsigned char>(0xC0 | codepoint >> 6));
-        charactor += (static_cast<unsigned char>(0x80 | codepoint & 0x3F));
-    }
-    else if(codepoint < 0x10000)
-    {
-        charactor += (static_cast<unsigned char>(0xE0 | codepoint >> 12));
-        charactor += (static_cast<unsigned char>(0x80 | codepoint >> 6 & 0x3F));
-        charactor += (static_cast<unsigned char>(0x80 | codepoint & 0x3F));
-    }
-    else
-    {
-        charactor += (static_cast<unsigned char>(0xF0 | codepoint >> 18));
-        charactor += (static_cast<unsigned char>(0x80 | codepoint >> 12& 0x3F));
-        charactor += (static_cast<unsigned char>(0x80 | codepoint >> 6 & 0x3F));
-        charactor += (static_cast<unsigned char>(0x80 | codepoint & 0x3F));
-    }
-    return charactor;
-}
-
-/* @brief unescape basic_string.          *
- * @param str inside of quatation marks   *
- * @return unescaped string.              */
-template<typename charT, typename traits, typename alloc>
-std::basic_string<charT, traits, alloc>
-unescape(const std::basic_string<charT, traits, alloc>& str)
-{
-    std::basic_string<charT, traits, alloc> retval;
-    for(typename std::basic_string<charT, traits, alloc>::const_iterator
-            iter = str.begin(); iter != str.end(); ++iter)
-    {
-        if(*iter != '\\')
-        {
-            retval += *iter;
-        }
-        else // *iter == '\'
-        {
-            ++iter;
-                 if(*iter == 'b') retval += '\b';
-            else if(*iter == 't') retval += '\t';
-            else if(*iter == 'n') retval += '\n';
-            else if(*iter == 'f') retval += '\f';
-            else if(*iter == 'r') retval += '\r';
-            else if(*iter == '\"') retval += '\"';
-            else if(*iter == '\\') retval += '\\';
-            else if(*iter == 'u')
-            {
-                std::string utf8code("0000");
-                for(std::size_t i=0; i<4; ++i)
-                {
-                    ++iter;
-                    utf8code += *iter;
-                }
-                retval += utf8_to_char(utf8code);
-            }
-            else if(*iter == 'U')
-            {
-                std::string utf8code;
-                for(std::size_t i=0; i<8; ++i)
-                {
-                    ++iter;
-                    utf8code += *iter;
-                }
-                retval += utf8_to_char(utf8code);
-            }
-        }
-    }
-    return retval;
-}
 
 // ---------------------------- implementation ---------------------------------
 
