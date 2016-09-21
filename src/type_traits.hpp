d715c8902c30f522c2a531cbdd1cc77dd7983c10
ToruNiina <niina.toru.68u@gmail.com>
2016-09-21 13:54:00 +0900

    change for c++98

--- src/type_traits.hpp
+++ src/type_traits.hpp
@@ -10,19 +10,25 @@ namespace toml
 template<typename T>
 struct is_toml_type : public std::false_type{};
 template<>
-struct is_toml_type<Boolean>  : public std::true_type{};
+struct is_toml_type<Boolean>   : public std::true_type{};
 template<>
-struct is_toml_type<Integer>  : public std::true_type{};
+struct is_toml_type<Integer>   : public std::true_type{};
 template<>
-struct is_toml_type<Float>    : public std::true_type{};
+struct is_toml_type<Float>     : public std::true_type{};
 template<>
-struct is_toml_type<String>   : public std::true_type{};
+struct is_toml_type<String>    : public std::true_type{};
 template<>
-struct is_toml_type<Datetime> : public std::true_type{};
+struct is_toml_type<Datetime>  : public std::true_type{};
 template<typename T>
-struct is_toml_type<Array<T>> : public std::true_type{};
+struct is_toml_type<Array<T> > : public std::true_type{};
 template<>                    
-struct is_toml_type<Table>    : public std::true_type{};
+struct is_toml_type<Table>     : public std::true_type{};
+
+#ifndef TOML_ENABLE_CXX11
+// in c++98 case, use std::vector as Array for simplicity.
+template<typename T>
+struct is_toml_type<std::vector<T>> : public std::true_type{};
+#endif
 
 template<typename T>
 struct is_toml_simple_type : public std::false_type{};
@@ -45,21 +51,21 @@ template<>
 struct is_toml_struct<table_type> : public std::true_type{};
 
 template<typename T>
-inline const char* print_type();
+inline const char* as_string();
 template<>
-inline const char* print_type<Boolean>(){return "Boolean";}
+inline const char* as_string<Boolean>(){return "Boolean";}
 template<>
-inline const char* print_type<Integer>(){return "Integer";}
+inline const char* as_string<Integer>(){return "Integer";}
 template<>
-inline const char* print_type<Float>(){return "Float";}
+inline const char* as_string<Float>(){return "Float";}
 template<>
-inline const char* print_type<String>(){return "String";}
+inline const char* as_string<String>(){return "String";}
 template<>
-inline const char* print_type<Datetime>(){return "Datetime";}
+inline const char* as_string<Datetime>(){return "Datetime";}
 template<>
-inline const char* print_type<array_type>(){return "array_type";}
+inline const char* as_string<array_type>(){return "array_type";}
 template<>
-inline const char* print_type<table_type>(){return "table_type";}
+inline const char* as_string<table_type>(){return "table_type";}
 
 }//toml
 
