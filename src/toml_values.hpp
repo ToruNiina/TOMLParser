9bce337c326992b8793d47a6e0109d9d8c072e46
ToruNiina <niina.toru.68u@gmail.com>
2016-09-21 13:54:24 +0900

    change toml_value_types for c++98

--- src/toml_values.hpp
+++ src/toml_values.hpp
@@ -1,5 +1,6 @@
 #ifndef TOML_VALUES
 #define TOML_VALUES
+#include "definitions.hpp"
 #include <vector>
 #include <string>
 #include <memory>
@@ -12,20 +13,20 @@ template<typename T>
 struct typed_value : public value_base
 {
     T value;
-    typed_value() = default;
-    ~typed_value() noexcept override = default;
+    typed_value(){}
+    ~typed_value() TOML_NOEXCEPT TOML_OVERRIDE {}
 };
 
 struct array_type : public value_base
 {
-    std::vector<std::shared_ptr<value_base>> value;
-    ~array_type() noexcept override = default;
+    std::vector<toml::shared_ptr<value_base> > value;
+    ~array_type() TOML_NOEXCEPT TOML_OVERRIDE {}
 };
 
 struct table_type : public value_base
 {
-    std::map<std::string, std::shared_ptr<value_base>> value;
-    ~table_type() noexcept override = default;
+    std::map<std::string, toml::shared_ptr<value_base> > value;
+    ~table_type() TOML_NOEXCEPT TOML_OVERRIDE {}
 };
 
 }//toml
