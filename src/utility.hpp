a4c73c8174af35f9dd599168b055256dd0fae83d
ToruNiina <niina.toru.68u@gmail.com>
2016-09-25 12:51:49 +0900

    move is_closed to utility

--- src/utility.hpp
+++ src/utility.hpp
@@ -20,5 +20,22 @@ std::size_t line_number(std::basic_istream<charT>& is)
     return lines;
 }
 
+template<typename charT>
+bool is_closed(const std::basic_string<charT>& str,
+               const charT open, const charT close)
+{
+    if(str.front() != open || str.back() != close) return false;
+
+    int counter = 0;
+    for(typename std::basic_string<charT>::const_iterator
+        iter = str.begin(); iter != str.end(); ++iter)
+    {
+        if(*iter == open) ++counter;
+        else if(*iter == close) --counter;
+    }
+    return counter == 0;
+}
+
+
 }// toml
 #endif /* TOML_UTILITY */
