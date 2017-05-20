47ec05750ed828fe077f3e75e49f9a834182bce7
ToruNiina <niina.toru.68u@gmail.com>
2017-05-20 17:35:21 +0900

    add toml::resize to utility

--- src/utility.hpp
+++ src/utility.hpp
@@ -1,55 +1,40 @@
 #ifndef TOML_UTILITY
 #define TOML_UTILITY
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <cstddef>
+#include <boost/mpl/bool.hpp>
+#include <boost/tti/has_member_function.hpp>
+#include <stdexcept>
 
 namespace toml
 {
-    
-template<typename charT>
-std::size_t line_number(std::basic_istream<charT>& is)
+
+namespace detail
 {
-    const typename std::basic_istream<charT>::pos_type position = is.tellg();
-    is.seekg(is.beg);
-    std::size_t lines = 1; // the first line is line 1.
-    while(is.tellg() != position)
-    {
-        if(is.get() == '\n') ++lines;
-    }
-    is.seekg(position);
-    return lines;
+
+BOOST_TTI_HAS_MEMBER_FUNCTION(resize)
+
+template<typename T>
+inline void resize_impl(T& container, std::size_t N, boost::mpl::true_)
+{
+    container.resize(N);
+    return;
 }
 
-template<typename charT>
-int read_integer(std::basic_istream<charT>& is, std::size_t digit)
+template<typename T>
+inline void resize_impl(T& container, std::size_t N, boost::mpl::false_)
 {
-    if(is.eof()) return 0;
-    int retval;
-    std::basic_string<charT> str;
-    for(std::size_t i=0; i<digit; ++i)
-    {
-        if(is.eof()) break;
-        str += is.get();
-    }
-    std::basic_istringstream<charT> iss(str);
-    iss >> retval;
-    return retval;
+    if(container.size() >= N) return;
+    else throw std::out_of_range("not resizable type not having enough size");
 }
 
-template<typename charT>
-std::basic_string<charT>
-get_numbers(std::basic_istream<charT>& is)
-{
-    std::basic_string<charT> retval;
-    while(!is.eof() && ('0' <= is.peek() && is.peek() <= '9'))
-    {
-        retval += is.get();
-    }
-    return retval;
-}
+}// detail
 
+template<typename T>
+inline void
+resize(T& container, std::size_t N)
+{
+    return detail::resize_impl(container, N, typename boost::mpl::bool_<
+        detail::has_member_function_resize<void (T::*)(std::size_t)>::value>());
+}
 
 }// toml
 #endif /* TOML_UTILITY */
